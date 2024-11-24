f38a5c16717f7b344708b2a8774faa26a057bf43
Abdul Hannan <152657898+AbdulHannan031@users.noreply.github.com>
2024-11-24 17:40:39 +0500

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,13 +4,7 @@ import {getAuth, GoogleAuthProvider} from 'firebase/auth';
 import {getStorage} from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBNgqKyDYM29hLjCEYv3ROKlb6hUsYOdQY",
-    authDomain: "ai-intervew-6d583.firebaseapp.com",
-    projectId: "ai-intervew-6d583",
-    storageBucket: "ai-intervew-6d583.appspot.com",
-    messagingSenderId: "513764379606",
-    appId: "1:513764379606:web:6745f3ab0bc93aa195f277",
-    measurementId: "G-KY4K2HVH4V"
+   //
 };
 
 const app = initializeApp(firebaseConfig);
